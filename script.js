d2c5d14e8531390313fddf058d3b7dd145b192bf
Rahul Tiwari <48430670+rahultiwari98@users.noreply.github.com>
2025-08-16 20:38:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Simple slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "flex";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
